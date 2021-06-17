9c26a9eb300dcfa9014b382d2574f24c9187b3c3
Andrea1801 <85683621+Andrea1801@users.noreply.github.com>
2021-06-17 16:51:09 +0200

    Create zad6.py

---
+++ zad6.py
@@ -0,0 +1,9 @@
+def rekurzivna_funkcija(s):
+    if len(s) ==0:
+        return s
+    else:
+        return rekurzivna_funkcija (s[1:])+s[0]
+
+rez=rekurzivna_funkcija("programiranje")
+print(rez)
+
